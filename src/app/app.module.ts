cab966dc7bbc6684ed65a3d55a157e19de8f56a6
Kian Mistry <kian-mistry@users.noreply.github.com>
2017-05-20 23:26:41 +0100

    Added dependencies for Angular Material.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,12 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
+import { NgModule } from '@angular/core';
+
+import 'hammerjs';
 
 import { AppComponent } from './components/app.component';
 
@@ -10,9 +15,12 @@ import { AppComponent } from './components/app.component';
 		AppComponent
 	],
 	imports: [
+		BrowserAnimationsModule,
 		BrowserModule,
+		FlexLayoutModule,
 		FormsModule,
-		HttpModule
+		HttpModule,
+		MaterialModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
